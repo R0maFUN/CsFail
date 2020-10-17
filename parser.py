7245df6ac026f004a86cfe60605532916a563b03
Roma Balayan <roma1balayan@gmail.com>
2020-10-17 14:21:55 +0300

    Work with database was added

--- parser.py
+++ parser.py
@@ -1,34 +1,27 @@
 from fake_useragent import UserAgent
-import requests
-from lxml import html
 from requests_html import HTMLSession
-import lxml.html
+import csv
 
-def GetKoefs(startID, roundsAmount):
+def GetKoefs(roundID, roundsAmount = 100, csv_filename = 'roundsData.csv'):
     ua = UserAgent()
     HEADERS = {
         'User-Agent': ua.opera
     }
     session = HTMLSession()
-    for i in range(roundsAmount):
-        startID-=1
-        startURL = 'https://cs.fail/crash/history/' + str(startID)
-        r = session.get(startURL, headers= HEADERS)
-        r.html.render(sleep=1)
-        #print(r.html.xpath('//meta[@name="description"]'))
-        roundData = r.html.search('content="Game #{gameID} crashed at x{gameKoef} with ${gameBank} bank!"')
-        print(f"Game #{roundData['gameID']}, koef = x{roundData['gameKoef']}")
-
-    #response = requests.get(startURL, headers = HEADERS, encoding = 'utf-8')
-    #response.request('https://cs.fail/runtime-es2015.1eba213af0b233498d9d.js')
-    #response.encoding = 'utf-8'
-    #print(response)
-    #parsed = html.fromstring(response.text)
-
-    #parsed1 = html.parse(response.text)
-    #print(parsed.find_class('main'))
-    #print(parsed.xpath('//a[@class="xhistory__link"]'))
+    with open(csv_filename, mode='w') as csv_file:
+        fieldnames = ['gameID', 'gameKoef', 'gameBank']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(roundsAmount):
+            roundURL = 'https://cs.fail/crash/history/' + str(roundID)
+            r = session.get(roundURL, headers=HEADERS)
+            r.html.render(sleep=1)
+            roundData = r.html.search('content="Game #{gameID} crashed at x{gameKoef} with ${gameBank} bank!"')
+            print(f"Game #{roundData['gameID']}, koef = x{roundData['gameKoef']}")
+            writer.writerow({key: roundData[key] for key in fieldnames})
+            print("[SAVED]")
+            roundID -= 1
 
 if __name__ == '__main__':
-    GetKoefs(699714, 15)
+    GetKoefs(699714, 5)
 
