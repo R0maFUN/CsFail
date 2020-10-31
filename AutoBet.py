84774db6e10cc891405ac87ba64f4d21bfbba954
Roma Balayan <roma1balayan@gmail.com>
2020-11-01 01:06:46 +0300

    Inventory and basic operations

--- AutoBet.py
+++ AutoBet.py
@@ -3,20 +3,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from fake_useragent import UserAgent
+from time import sleep
+#from fake_useragent import UserAgent
 
 # class Round:
 #     def __init__(self):
 #         self.koef = 0.0
-# class Skin:
-#
-# class Inventory:
-#
+class Skin:
+    def __init__(self, htmlObject):
+        self.htmlObj = htmlObject
+        self.price = htmlObject.find_element_by_class_name("skin__coins").text
+        self.price = float(self.price.strip('$'))
+        self.isSelected = False
+
+class Inventory:
+    def __init__(self, driver):
+        invHtml = driver.find_elements_by_class_name("skin_theme_inventory")
+        self.skins = [Skin(el) for el in invHtml]
+        self.balance = sum(el.price for el in self.skins)
+        self.selectedSkins = list()
+        self.selectedSkinsPrice = 0
+    def SelectSkin(self, price):
+        for skin in self.skins:
+            if skin.price == price and skin.isSelected == False:
+                skin.htmlObj.click()
+                skin.isSelected = True
+                self.selectedSkins.append(skin)
+                self.selectedSkinsPrice += skin.price
+                return
+    def UnSelectSkin(self, price):
+        for skin in self.skins:
+            if skin.price == price and skin.isSelected == True:
+                skin.htmlObj.click()
+                skin.isSelected = False
+                self.selectedSkins.remove(skin)
+                self.selectedSkinsPrice -= skin.price
+                return
+    def SelectAll(self):
+        for skin in self.skins:
+            if skin.isSelected == False:
+                skin.htmlObj.click()
+                skin.isSelected = True
+                self.selectedSkins.append(skin)
+                self.selectedSkinsPrice += skin.price
+                sleep(0.5)
+    def UnSelectAll(self):
+        for skin in self.skins:
+            if skin.isSelected == False:
+                skin.htmlObj.click()
+                skin.isSelected = True
+                self.selectedSkins.append(skin)
+                self.selectedSkinsPrice += skin.price
+                sleep(0.5)
 
 def waitForStrick(driver, crushStrick = 1): # Getting latest round and checking for X crash strick
     lastRound = driver.find_element_by_class_name('xhistory__link')
-    lastRoundKoef, lastRoundId = lastRound.text, int(lastRound.get_attribute('href').split('/')[-1])
 
+    lastRoundKoef, lastRoundId = lastRound.text, int(lastRound.get_attribute('href').split('/')[-1])
     crushes = 0
     while(1):
         lastRound = WebDriverWait(driver, 300).until(
@@ -63,6 +106,15 @@ print("Make an authorization")
 while(input("Did you log in?[y/n]") != 'y'):
     continue
 
+test = Inventory(driver)
+for sk in test.skins:
+    print(sk.price)
 
+test.SelectSkin(price=0.25)
+sleep(2)
+test.SelectAll()
+sleep(2)
+test.UnSelectSkin(price=2.49)
+sleep(2)
 #Main program
 waitForStrick(driver)
