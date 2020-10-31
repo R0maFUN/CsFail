e86b2085902a23b0cf2008b3e0aa5324ee557add
Roma Balayan <roma1balayan@gmail.com>
2020-11-01 00:03:00 +0300

    Authorization and chrome options

--- AutoBet.py
+++ AutoBet.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 
 # class Round:
 #     def __init__(self):
@@ -11,7 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 # class Inventory:
 #
 
-def nameforfun(driver, crushStrick = 1): # Getting latest round and checking for X crash strick
+def waitForStrick(driver, crushStrick = 1): # Getting latest round and checking for X crash strick
     lastRound = driver.find_element_by_class_name('xhistory__link')
     lastRoundKoef, lastRoundId = lastRound.text, int(lastRound.get_attribute('href').split('/')[-1])
 
@@ -31,7 +33,22 @@ def nameforfun(driver, crushStrick = 1): # Getting latest round and checking for
 
 
 
-driver = webdriver.Chrome()
+chrome_options = Options() # saving and loading cookies/settings/etc and trying to make selenium undetectable
+
+#ua = UserAgent()
+#userAgent = ua.random
+#print(userAgent)
+chrome_options.add_argument("start-maximized")
+#chrome_options.add_argument(f'user-agent={userAgent}')
+chrome_options.add_argument("user-data-dir=selenium")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+
+#driver = webdriver.Chrome()
 driver.get('http://cs.fail')
 
 try:
@@ -41,4 +58,11 @@ try:
 except:
     driver.quit()
 
-nameforfun(driver)
+#Authorization
+print("Make an authorization")
+while(input("Did you log in?[y/n]") != 'y'):
+    continue
+
+
+#Main program
+waitForStrick(driver)
